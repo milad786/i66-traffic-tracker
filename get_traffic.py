ce46847753624295429f0e7917b8a74ca53ac92e
Ahmad Milad Salangi <asalang@gmu.edu>
2026-08-11 16:05:49 -0400

    Connect API script to database, full pipeline working

--- get_traffic.py
+++ get_traffic.py
@@ -1,6 +1,10 @@
 import os
 import requests
 from dotenv import load_dotenv
+from datetime import datetime
+now = datetime.now()
+timestamp_str = now.strftime("%Y-%m-%d %H:%M:%S")
+from database import save_reading
 
 load_dotenv()
 
@@ -17,5 +21,20 @@ params = {
 
 response = requests.get(url, params=params)
 
+data = response.json()
+flow_data = data["flowSegmentData"]
+current_speed = flow_data["currentSpeed"]
+free_flow_speed = flow_data["freeFlowSpeed"]
+confidence = flow_data['confidence']
+is_congested = current_speed < (free_flow_speed * 0.8)
+
+
+
 print("Status code:", response.status_code)
-print(response.json())
+
+print(timestamp_str)
+
+print(current_speed)
+print(free_flow_speed)
+print(confidence)
+save_reading(timestamp_str, current_speed, free_flow_speed, confidence, is_congested)
