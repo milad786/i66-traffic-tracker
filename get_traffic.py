8fd56f28571148d9d13a6368801e50764b357392
Ahmad Milad Salangi <asalang@gmu.edu>
2026-08-12 15:37:02 -0400

    Clean up and setting interval for 5 mins

--- get_traffic.py
+++ get_traffic.py
@@ -2,39 +2,46 @@ import os
 import requests
 from dotenv import load_dotenv
 from datetime import datetime
-now = datetime.now()
-timestamp_str = now.strftime("%Y-%m-%d %H:%M:%S")
 from database import save_reading
+import time
 
-load_dotenv()
+def fetch_and_save():# move your existing logic here:
+    now = datetime.now()
+    timestamp_str = now.strftime("%Y-%m-%d %H:%M:%S")
+    load_dotenv()
+    
+    API_KEY = os.getenv("TOMTOM_API_KEY")
+    LATITUDE = 38.8815
+    LONGITUDE = -77.2247
+    
+    url = "https://api.tomtom.com/traffic/services/4/flowSegmentData/absolute/10/json"
+    params = {
+        "key": API_KEY,
+        "point": f"{LATITUDE},{LONGITUDE}"
+        }
 
-API_KEY = os.getenv("TOMTOM_API_KEY")
+    response = requests.get(url, params=params)
 
-LATITUDE = 38.8815
-LONGITUDE = -77.2247
+    data = response.json()
 
-url = "https://api.tomtom.com/traffic/services/4/flowSegmentData/absolute/10/json"
-params = {
-    "key": API_KEY,
-    "point": f"{LATITUDE},{LONGITUDE}"
-}
-
-response = requests.get(url, params=params)
-
-data = response.json()
-flow_data = data["flowSegmentData"]
-current_speed = flow_data["currentSpeed"]
-free_flow_speed = flow_data["freeFlowSpeed"]
-confidence = flow_data['confidence']
-is_congested = current_speed < (free_flow_speed * 0.8)
+    flow_data = data["flowSegmentData"]
+    current_speed = flow_data["currentSpeed"]
+    free_flow_speed = flow_data["freeFlowSpeed"]
+    confidence = flow_data['confidence']
+    is_congested = current_speed < (free_flow_speed * 0.8)
 
 
+    print("Status code:", response.status_code)
 
-print("Status code:", response.status_code)
+    print(timestamp_str)
+    print(current_speed)
+    print(free_flow_speed)
+    print(confidence)
 
-print(timestamp_str)
+    save_reading(timestamp_str, current_speed, free_flow_speed, confidence, is_congested)
 
-print(current_speed)
-print(free_flow_speed)
-print(confidence)
-save_reading(timestamp_str, current_speed, free_flow_speed, confidence, is_congested)
+    pass
+
+while True:
+    fetch_and_save()
+    time.sleep(300)  
